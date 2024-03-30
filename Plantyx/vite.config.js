a7bf3816cc66793fab57d7dd8b37c03e42055ab9
unknown <abiodunfatunbi4@gmail.com>
2024-03-30 01:54:46 +0100

    fixed config file

--- Plantyx/vite.config.js
+++ Plantyx/vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.json'], // Add .jsx extension
   },
-  build: {
-    rollupOptions:{
-      input: './src/main.jsx'
-    }
-  }
+ 
 })
